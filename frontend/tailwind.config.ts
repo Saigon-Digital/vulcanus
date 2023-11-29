7fb114f18df04aa8c4af81e5b85475782aaf70e2
duynd243 <duyndse@gmail.com>
2023-11-29 17:42:39 +0700

    chore: setup tailwind colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          blue: {
+            main: "#009EE0",
+            400: "#4CBBE9",
+            300: "#80CEF0",
+            200: "#B3E2F6",
+            100: "#E5F5FC",
+          },
+          midBlue: {
+            main: "#004594",
+            400: "#4C7DB4",
+            300: "#80A2C9",
+            200: "#B3C7DF",
+            100: "#E5ECF4",
+          },
+          black: {
+            main: "#140F24",
+          },
+        },
+        secondary: {
+          offWhite: {
+            main: "#E6ECF3",
+          },
+          green: "#179C6F",
+          yellow: "#F7CC5F",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
