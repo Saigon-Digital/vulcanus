5f144fdee0643f23348529fbfe971bbe059bb134
PhuocMinh111 <phuoc@saigon.digital>
2023-11-30 16:37:54 +0700

    hero component

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -58,6 +58,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/container-queries")],
+  plugins: [require("@tailwindcss/container-queries"),
+
+],
 };
 export default config;
