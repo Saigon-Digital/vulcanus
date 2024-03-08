68d1a6cc46b16088ec92364d649d71f815e7ae66
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-03-08 14:35:01 +0700

    CTA for blog page

--- frontend/src/components/BlogsBlock/index.tsx
+++ frontend/src/components/BlogsBlock/index.tsx
@@ -42,20 +42,21 @@ interface Props extends BlogsBlockFragment {
 }
 const BlogsBlock = ({blockListing = DEFAULT_BLOG}: Props) => {
   return (
-    <div className="container-fluid py-20 lg:py-28">
+    <section className="container-fluid py-20 lg:py-28">
       <div className="grid grid-cols-12">
-        <div className="col-span-full flex flex-col gap-10 md:col-span-8">
+        <div className="col-span-full flex  flex-col gap-10 md:col-span-8">
           {blockListing.map((ele, id) => {
             return (
-              <div key={id} className=" flex min-h-[350px] gap-5 ">
-                <Image
-                  width={608}
-                  height={400}
-                  className="aspect-[608/400] h-full max-h-[400px] w-1/2 flex-grow object-contain"
-                  src={ele.featureImage || ""}
-                  alt="blog iamge"
-                />
-                <div className="flex flex-col justify-center gap-2">
+              <div key={id} className=" flex flex-wrap gap-5 lg:min-h-[350px] ">
+                <div className="relative min-h-[250px] w-full md:w-[45%]">
+                  <Image
+                    fill
+                    className=" max-h-[400px] w-full object-cover"
+                    src={ele.featureImage || ""}
+                    alt="blog image"
+                  />
+                </div>
+                <div className="flex w-full flex-col justify-center gap-2 md:w-1/2">
                   <h4 className="text-xl uppercase text-primary-blue-main">
                     {ele.category}
                   </h4>
@@ -73,8 +74,31 @@ const BlogsBlock = ({blockListing = DEFAULT_BLOG}: Props) => {
             );
           })}
         </div>
+        <div className="col-span-full mt-10 md:col-span-6 lg:col-span-3 lg:col-start-10 lg:mt-0">
+          <div className="flex w-full flex-col gap-4 rounded-md border border-primary-blue-main p-6">
+            <h3 className="text-2xl text-primary-blue-main ">Get in touch</h3>
+            <p>
+              We are your partner for CNC manufacturing of individual parts and
+              series, specialised constructions, and repair work.
+            </p>
+            <Button as="link" className="mt-4" href="#">
+              Get in touch
+            </Button>
+          </div>
+          <div className="mt-6 flex w-full flex-col gap-4 rounded-md border bg-white p-6">
+            <h3 className="text-2xl text-primary-blue-main ">Get in touch</h3>
+            <p className="text-black">
+              Please apply in writing, preferably by e-mail, to:
+              bewerbung@vulcanus-stahl.deWe look forward to receiving your
+              applications.
+            </p>
+            <Button as="link" className="mt-4" href="#">
+              work with us
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -14,7 +14,7 @@ const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, Props>(
   (props, ref) => {
     const commonProps = {
       ...props,
-
+      style: {width: "fit-content"},
       className: twMerge(
         "inline-flex items-center fit-content justify-center text-secondary-offWhite-white lg:px-[60px] px-10 py-[17px] bg-primary-blue-main uppercase leading-[125%] text-center hover:bg-primary-blue-400 font-bold transition-all duration-300",
         props.className
