724eb3e3228f04ea976e191c4caa466bfafcea03
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-03-14 18:14:20 +0700

    add border button service

--- frontend/src/components/TitleBlock/index.tsx
+++ frontend/src/components/TitleBlock/index.tsx
@@ -4,7 +4,7 @@ import parse, {HTMLReactParserOptions, Element} from "html-react-parser";
 
 const TitleBlock: React.FC<TitleBlockFragment> = ({title}) => {
   return (
-    <div className="title-block container py-16 lg:py-20">
+    <div className="title-block container border-b border-[#E6ECF3] py-16 lg:py-20">
       <div className="max-w-[1000px] text-5xl font-bold">
         {title && parse(title)}
       </div>
