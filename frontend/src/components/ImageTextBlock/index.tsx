a63cc9bbfde8ae136b622160f79be45cc0cb5c8a
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-03-15 12:36:27 +0700

    update typography image text block

--- frontend/src/components/ImageTextBlock/index.tsx
+++ frontend/src/components/ImageTextBlock/index.tsx
@@ -9,7 +9,7 @@ const ImageText: React.FC<ImageTextBlock> = ({
   return (
     <div className="container py-20">
       <div className="grid grid-cols-4 gap-x-5 gap-y-10">
-        <p className="col-span-full md:col-span-2  md:col-start-3">
+        <p className="col-span-full font-[300] md:col-span-2 md:col-start-3  lg:text-xl">
           {textRepeater && textRepeater[0]?.text}
         </p>
         <div className="relative col-span-full h-[360px] md:col-span-2 lg:col-span-1 ">
@@ -31,7 +31,11 @@ const ImageText: React.FC<ImageTextBlock> = ({
         <div className="col-span-full flex gap-5 md:col-span-2 md:col-start-3">
           {textRepeater &&
             textRepeater.slice(1, textRepeater.length).map((ele, id) => {
-              return <p key={id}>{ele?.text}</p>;
+              return (
+                <p className=" font-[300] lg:text-xl" key={id}>
+                  {ele?.text}
+                </p>
+              );
             })}
         </div>
       </div>
