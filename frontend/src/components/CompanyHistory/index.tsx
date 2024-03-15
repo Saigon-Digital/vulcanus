9fbef081a1cba100e3e23ba9e0f812c6f945b136
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-03-15 12:34:38 +0700

    update mobile responsive history block

--- frontend/src/components/CompanyHistory/index.tsx
+++ frontend/src/components/CompanyHistory/index.tsx
@@ -2,22 +2,33 @@ import React, {useMemo, useState} from "react";
 import {CompanyHistoryBlock} from "@/__generated__/graphql";
 import Image from "next/image";
 import {Swiper, SwiperSlide, SwiperClass} from "swiper/react";
-
+import {useMediaQuery} from "@/hooks/useMediaQuery";
 import {Pagination} from "swiper/modules";
 import clsx from "clsx";
 
 // import "./index.scss";
 const CompanyHistory = (props: CompanyHistoryBlock) => {
-  const activeSlide = props.histories
-    ? Math.floor(props.histories?.length / 2) - 1
+  const isMobile = useMediaQuery("(max-width:768px)");
+  const activeSlide = isMobile
+    ? props.histories
+      ? Math.floor(props.histories?.length / 2) - 1
+      : 0
     : 0;
 
-  const [currentSlide, setCurentSlide] = useState<number>(activeSlide);
+  const [currentSlide, setCurentSlide] = useState<number | undefined>(
+    activeSlide
+  );
   // console.log();
 
   const onSwiperChange = (swiper: SwiperClass) => {
-    console.log("swiper change ", swiper.activeIndex);
+    // console.log("swiper change ", swiper.activeIndex);
     const slide = swiper.activeIndex;
+    if (isMobile)
+      //@ts-ignore
+      return setCurentSlide((prev: number) => {
+        if (slide - 1 < 0) return 0;
+        return slide - 1;
+      });
     setCurentSlide(slide);
   };
 
@@ -26,38 +37,44 @@ const CompanyHistory = (props: CompanyHistoryBlock) => {
       props.histories && props.histories.find((ele, id) => id === currentSlide)
     );
   }, [currentSlide]);
-  console.log(currentSlide);
-
-  console.log(activeHistory);
 
   return (
     <div
       className={`history container-fluid rounded-xl bg-primary-midBlue-main px-5 py-14 lg:px-12 lg:py-20`}>
       <div className="grid grid-cols-12 gap-5">
-        <div className="col-span-full flex flex-col md:col-span-4 lg:col-span-3">
-          <h2 className="text-4xl font-bold lg:text-5xl xl:text-[64px]">
+        <div className="col-span-full grid grid-cols-6 md:col-span-4 md:mt-0 lg:col-span-3">
+          <h2 className="col-span-full text-4xl font-bold lg:text-5xl xl:text-[64px]">
             {props.title}
           </h2>
-          <div className="relative mt-10 flex w-[134px] gap-4  after:absolute after:top-1/2 after:h-14 after:w-full after:-translate-y-1/2 after:rounded-md after:bg-primary-blue-main md:flex-col md:justify-end lg:ml-auto ">
+
+          <div className="relative col-span-2  mt-10  flex w-[65px] gap-4 after:absolute after:top-0 after:h-14 after:w-full after:rounded-md after:bg-primary-blue-main md:col-span-full md:w-[134px] md:flex-col md:justify-end after:md:top-1/2 after:md:-translate-y-1/2 lg:ml-auto ">
             <div className=" w-full">
               <Swiper
                 modules={[Pagination]}
                 grabCursor={true}
                 direction="vertical"
                 initialSlide={activeSlide}
-                centeredSlides={true}
+                centeredSlides={isMobile ? false : true}
                 onSlideChange={(swiper) => onSwiperChange(swiper)}
                 slidesPerView={5}>
                 {props.histories?.map((ele, id) => {
                   return (
                     <SwiperSlide
                       className={clsx(
-                        ` !flex items-center justify-center  text-right `,
+                        ` !flex items-center justify-center text-right text-xl  xl:text-3xl `,
                         activeSlide === id && "slide-active",
-                        id === currentSlide - 2 && "opacity-40",
-                        id === currentSlide - 1 && "opacity-80",
-                        id === currentSlide + 1 && "opacity-80",
-                        id === currentSlide + 2 && "opacity-40"
+                        id === currentSlide
+                          ? currentSlide - 2 && "opacity-40"
+                          : null,
+                        id === currentSlide
+                          ? currentSlide - 1 && "opacity-80"
+                          : null,
+                        id === currentSlide
+                          ? currentSlide + 1 && "opacity-80"
+                          : null,
+                        id === currentSlide
+                          ? currentSlide + 2 && "opacity-40"
+                          : null
                       )}
                       key={id}>
                       {ele?.year}
@@ -67,8 +84,21 @@ const CompanyHistory = (props: CompanyHistoryBlock) => {
               </Swiper>
             </div>
           </div>
+          <div className="col-span-4 mt-10 md:hidden">
+            <Image
+              className="aspect-video w-full rounded-md object-cover"
+              alt="history"
+              width={920}
+              height={540}
+              src={
+                activeHistory?.mainImage?.node.sourceUrl ||
+                "/images/hero-banner.png"
+              }
+            />
+            <div className="mt-5">{activeHistory?.description}</div>
+          </div>
         </div>
-        <div className="col-span-full grid grid-cols-12 gap-4 md:col-span-8 md:gap-5">
+        <div className="col-span-full hidden grid-cols-12 gap-4 md:col-span-8 md:grid md:gap-5">
           <div className="col-span-3">
             <Image
               className="ml-auto aspect-[316/267] rounded-md object-cover lg:w-3/4"
