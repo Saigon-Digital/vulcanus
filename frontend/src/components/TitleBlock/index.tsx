d0cded6b22e2097bc3849feb45925d7fb21f7f7a
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-03-19 13:35:08 +0700

    fix title block

--- frontend/src/components/TitleBlock/index.tsx
+++ frontend/src/components/TitleBlock/index.tsx
@@ -9,9 +9,9 @@ const TitleBlock: React.FC<TitleBlockFragment> = ({
   haveBorderBottom,
 }) => {
   return (
-    <section className="relative">
+    <section className="container-fluid relative">
       <div
-        className={`title-block container-fluid  grid grid-cols-12 py-16 lg:py-20 ${
+        className={`title-block   grid grid-cols-12 py-16 lg:py-20 ${
           haveBorderBottom && " border-b border-[#E6ECF3]"
         }`}>
         <div className="title col-span-full max-w-[1000px] text-4xl font-bold md:col-span-8 xl:text-5xl 2xl:leading-[67px]   3xl:col-span-7 [&>*]:text-4xl xl:[&>*]:text-5xl 2xl:[&>*]:leading-[67px]">
