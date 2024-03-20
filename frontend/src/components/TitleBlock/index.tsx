7ef30bd119d8b681474d04c6f6f869817386ebff
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-03-20 14:45:11 +0700

    increase title length

--- frontend/src/components/TitleBlock/index.tsx
+++ frontend/src/components/TitleBlock/index.tsx
@@ -14,14 +14,14 @@ const TitleBlock: React.FC<TitleBlockFragment> = ({
         className={`title-block   grid grid-cols-12 py-16 lg:py-20 ${
           haveBorderBottom && " border-b border-[#E6ECF3]"
         }`}>
-        <div className="title col-span-full max-w-[1000px] text-4xl font-bold md:col-span-8 xl:text-5xl 2xl:leading-[67px]   3xl:col-span-7 [&>*]:text-4xl xl:[&>*]:text-5xl 2xl:[&>*]:leading-[67px]">
+        <div className="title col-span-full max-w-[1165px] text-4xl font-bold md:col-span-8 xl:text-5xl 2xl:leading-[67px]   3xl:col-span-8 [&>*]:text-4xl [&>*]:tracking-tight xl:[&>*]:text-5xl 2xl:[&>*]:leading-[67px]">
           {title && parse(title)}
         </div>
         {haveShape && (
           <TitleShape
             h={200}
             w={200}
-            className="absolute -right-5 top-1/2 hidden -translate-y-1/2 lg:block"
+            className="absolute -right-[10px] top-1/2 hidden -translate-y-1/2 lg:block"
           />
         )}
       </div>
