b1650a6524a6ad3f8e70035a10815193457e6235
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-04-02 15:25:00 +0700

    remove getStaticPath preview

--- frontend/src/pages/preview.tsx
+++ frontend/src/pages/preview.tsx
@@ -4,9 +4,3 @@ import {GetStaticProps} from "next";
 export default function Preview(props: WordPressTemplateProps) {
   return <WordPressTemplate {...props} />;
 }
-
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  return getWordPressProps({
-    ctx,
-  });
-};
