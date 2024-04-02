225c037a101f3119964bf05aecc74c93357fc91d
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-04-02 16:26:04 +0700

    remove middleware

--- frontend/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import type {NextRequest} from "next/server";
-import {NextResponse} from "next/server";
-export const middleware = (req: NextRequest) => {
-  const nextUrl = req.nextUrl;
-  if (nextUrl.pathname.startsWith("//")) {
-    nextUrl.pathname.replace("//", "/");
-    return NextResponse.rewrite(nextUrl);
-  }
-};
