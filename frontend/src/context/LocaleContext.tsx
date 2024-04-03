cdf98c5bf57b128944c0eaf458b52ef8287e478e
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-04-03 18:01:27 +0700

    hide button if langauge not avalaible

--- frontend/src/context/LocaleContext.tsx
+++ frontend/src/context/LocaleContext.tsx
@@ -1,13 +1,22 @@
-import {createContext, useContext} from "react";
+import {createContext, PropsWithChildren, useContext} from "react";
 type LocaleContextType = {
-  databaseId?: number;
-  localeData?:any
+  databaseId?: number | null;
+  localeData?: {
+    EN: string | null;
+    DE: string | null;
+  } | null;
 };
-const LocaleContext = createContext<LocaleContextType | any>(null);
+const LocaleContext = createContext<LocaleContextType>({
+  databaseId: null,
+  localeData: {DE: null, EN: null},
+});
 
-export const LocaleContextProvider = ({children, localeData}:any) => {
+export const LocaleContextProvider = ({
+  children,
+  localeData,
+}: PropsWithChildren<LocaleContextType>) => {
   return (
-    <LocaleContext.Provider value={{localeData:localeData}}>
+    <LocaleContext.Provider value={{localeData: localeData}}>
       {children}
     </LocaleContext.Provider>
   );
