9b37dce2342729d83675cb76cca7be248b38aede
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-04-09 12:58:50 +0700

    animation for title block

--- frontend/src/components/TitleText/index.tsx
+++ frontend/src/components/TitleText/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Shape from "./images/shape.svg";
+import {motion} from "framer-motion";
 type TText = {
   title?: string;
 };
@@ -9,9 +10,13 @@ const index = ({
   return (
     <section className="container-fluid relative  gap-4 py-14">
       <div className="relative grid grid-cols-12">
-        <h2 className="title-xl col-span-10 md:col-span-8 lg:col-span-7">
+        <motion.h2
+          initial={{y: 30, opacity: 0}}
+          whileInView={{y: 0, opacity: 1}}
+          transition={{duration: 0.5}}
+          className="title-xl col-span-10 md:col-span-8 lg:col-span-7">
           {title}
-        </h2>
+        </motion.h2>
         <Shape className="absolute right-0 hidden aspect-square h-1/3 object-contain sm:top-1/2 sm:h-2/3 sm:-translate-y-1/2 md:block lg:h-full" />
       </div>
     </section>
--- frontend/src/hooks/useImageCss.ts
+++ frontend/src/hooks/useImageCss.ts
@@ -36,7 +36,9 @@ function useImageStyle({
       width: w,
       height: h,
       src: src,
-
+      // placeholder: "blur",
+      // blurDataURL:
+      //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAECAYAAAC3OK7NAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAACxMAAAsTAQCanBgAAACjSURBVBhXHcpNC8FgAMDx/4O8rBS1HBTKjbgpVw6+jM+nfAEX4sJyELUaY83mvT1etodcf/3E2HypdEJxfoLhK+o5cIOInWUxNGyihEayUEUs7KeKx8Q/Ov6dyXSOeQlxUnm2P6yWK4T+ATEwLmo5G+Hd3oQZHfmS8AnwfImSJ0qa5O5YCL3WUw93TavTI6uXeRxWdNsNNkGRfvNK5r3ndLzyBV2qUIUDrZ3fAAAAAElFTkSuQmCC",
       objectFit: "cover",
     });
     const bgImageStyle = getBackgroundImage(srcSet);
