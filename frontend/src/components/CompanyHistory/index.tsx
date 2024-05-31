1c10415c71fc038d754a7f57529ef29911a69ef7
Khoa Phan <pldkhoa96.work@gmail.com>
2024-05-31 11:51:09 +0700

    refactor: layout and animation history block

--- frontend/src/components/CompanyHistory/index.tsx
+++ frontend/src/components/CompanyHistory/index.tsx
@@ -1,4 +1,5 @@
 import React, {
+  Dispatch,
   PropsWithChildren,
   useEffect,
   useMemo,
@@ -15,32 +16,40 @@ import {
   SwiperRef,
 } from "swiper/react";
 import {useMediaQuery} from "@/hooks/useMediaQuery";
-import {Pagination} from "swiper/modules";
+import {Mousewheel} from "swiper/modules";
 import clsx from "clsx";
+import {Maybe} from "graphql/jsutils/Maybe";
 
-// import Draggable from "react-draggable";
+type Histories = NonNullable<CompanyHistoryBlock["histories"]>;
 
-// import "./index.scss";
+interface HistoryObject {
+  [key: string]: Histories[number];
+}
+
 const CompanyHistory = (props: CompanyHistoryBlock) => {
   // Slider not work as expected, check logic again
-  const isMobile = useMediaQuery("(max-width:768px)");
   let sizes = props.histories ? props.histories.length : 0;
-  const activeSlide = !isMobile
-    ? sizes > 5
-      ? 2
-      : Math.floor(sizes / 2) - 1
-    : 0;
+  function transformArray(arr: Histories): HistoryObject[] | null {
+    if (!arr) return null;
+    if (arr?.length === 0) return [];
 
-  const [currentSlide, setCurentSlide] = useState<number | undefined>(
-    activeSlide
-  );
-  let histories = props.histories ? [...props.histories] : [];
+    const result: Array<HistoryObject> = [];
+    arr.forEach((el, i) => {
+      const pre = i === 0 ? arr[0] : arr?.[i - 1];
+      const current = arr[i];
+      if (pre && current) {
+        result.push({pre: pre, current: current});
+      }
+    });
+    return result;
+  }
 
-  // return;
-  histories.sort((a, b) => (a?.year && b?.year ? a?.year - b?.year : -1));
+  const imagePairs = props.histories && transformArray(props.histories);
+  const [imgSlideHeight, setImgSlideHeight] = useState(0);
 
-  const [mobileSlide, setMobileSlide] =
-    useState<typeof props.histories>(histories);
+  const initialSlide = Math.floor(sizes / 2) + 1;
+
+  const [currentSlide, setCurentSlide] = useState<number>(initialSlide);
 
   const swiperRef = useRef<SwiperRef | null>(null);
 
@@ -50,95 +59,51 @@ const CompanyHistory = (props: CompanyHistoryBlock) => {
     setCurentSlide(slide);
   };
 
-  const activeHistory = useMemo(() => {
-    if (isMobile) {
-      return mobileSlide?.at(0);
-    }
-    return histories && histories.find((ele, id) => id === currentSlide);
-  }, [currentSlide, mobileSlide]);
-
-  const prevHistory = useMemo(() => {
-    if (isMobile) return mobileSlide?.at(0);
-    if (currentSlide && currentSlide - 1 > 0) {
-      return histories && histories.find((ele, id) => id === currentSlide - 1);
-    } else {
-      return histories && histories.find((ele, id) => id === currentSlide);
-    }
-  }, [currentSlide]);
-
-  const onMobileSlide = (index: number) => {
-    let result = mobileSlide ? [...mobileSlide] : [];
-
-    let rightItems = result.filter((ele, id) => id < index);
-    let leftItem = result.filter((ele, id) => id >= index);
-
-    result = [...leftItem, ...rightItems];
-
-    setMobileSlide(result);
-  };
-  // console.log(mobileSlide);
-
   return (
     <div
       className={`history container-fluid rounded-xl bg-primary-midBlue-main px-5 py-14 lg:px-12 lg:py-20`}>
       <div className="grid grid-cols-12 gap-5">
-        <div className="col-span-full grid grid-cols-6 md:col-span-4 md:mt-0 lg:col-span-3">
+        <div className="col-span-full md:col-span-4 lg:col-span-3">
           <h2 className="col-span-full text-4xl font-bold lg:text-5xl xl:text-[64px]">
             {props.title}
           </h2>
 
-          <div className="relative col-span-2  mt-10  flex  w-[65px] gap-4 after:absolute after:top-0 after:h-14 after:w-full after:rounded-md after:bg-primary-blue-main sm:h-[360px] md:col-span-full md:w-[134px] md:flex-col md:justify-end after:md:top-1/2 after:md:-translate-y-1/2 lg:ml-auto ">
-            <div className="z-10 flex w-full flex-col items-center justify-start md:hidden">
-              {mobileSlide?.map((ele, id) => {
-                return (
-                  <div
-                    key={id}
-                    onClick={() => onMobileSlide(id)}
-                    className="flex h-14 w-full items-center justify-center">
-                    {ele?.year}
-                  </div>
-                );
-              })}
-            </div>
-
-            <div className=" hidden w-full md:block">
+          <div className="relative col-span-2 mt-6 w-full gap-4 md:col-span-full md:w-fit md:flex-col md:justify-end lg:ml-auto ">
+            <div className="w-full">
               <Swiper
-                modules={[Pagination]}
-                grabCursor={true}
-                direction="vertical"
-                // draggable
-                // freeMode
-                className="h-[300%] sm:h-full"
-                // simulateTouch
+                modules={[Mousewheel]}
+                mousewheel={true}
+                breakpoints={{
+                  320: {
+                    slidesPerView: 3,
+                    direction: "horizontal",
+                  },
+                  640: {
+                    slidesPerView: 5,
+                  },
+                  768: {
+                    slidesPerView: 7,
+                    direction: "vertical",
+                  },
+                }}
                 ref={swiperRef}
-                initialSlide={activeSlide}
-                centeredSlides={!isMobile}
+                initialSlide={initialSlide}
+                centeredSlides={true}
                 onSlideChange={(swiper: any) => onSwiperChange(swiper)}
-                slidesPerView={isMobile ? 4 : 5}>
-                {histories?.map((ele, id) => {
+                slidesPerView={"auto"}>
+                {props?.histories?.map((ele, id) => {
+                  const distanceWithActive = currentSlide - id;
+                  const isActive = id === currentSlide;
                   return (
                     <SwiperSlide
                       onClick={(e) => {
-                        if (isMobile) setCurentSlide(id);
+                        setCurentSlide(id);
                       }}
-                      // onClickCapture={}
-
-                      className={clsx(
-                        ` !flex items-center justify-center text-right text-xl  xl:text-3xl `,
-                        activeSlide === id && "slide-active",
-                        currentSlide &&
-                          id === currentSlide - 2 &&
-                          "!hidden opacity-40 sm:!block xl:text-[24px]",
-                        currentSlide &&
-                          id === currentSlide - 1 &&
-                          "!hidden opacity-80 sm:!block",
-                        currentSlide &&
-                          id === currentSlide + 2 &&
-                          "opacity-40 xl:text-[24px]",
-                        currentSlide && id === currentSlide + 1 && "opacity-80 "
-                      )}
                       key={id}>
-                      <SlideButton isMobile={isMobile} id={id}>
+                      <SlideButton
+                        distanceWithActive={distanceWithActive}
+                        isActive={isActive}
+                        id={id}>
                         {ele?.year}
                       </SlideButton>
                     </SwiperSlide>
@@ -147,70 +112,127 @@ const CompanyHistory = (props: CompanyHistoryBlock) => {
               </Swiper>
             </div>
           </div>
-          <div className="col-span-4 mt-10 md:hidden">
-            <Image
-              className="aspect-video w-full rounded-md object-cover"
-              alt="history"
-              width={920}
-              height={540}
-              src={
-                activeHistory?.mainImage?.node.sourceUrl ||
-                "/images/hero-banner.png"
-              }
-            />
-            <div className="mt-5">{activeHistory?.description}</div>
-          </div>
         </div>
-        <div className="col-span-full hidden grid-cols-12 gap-4 md:col-span-8 md:grid md:gap-5 lg:col-start-5">
-          <div className="col-span-3">
-            <Image
-              className="ml-auto aspect-[316/267] rounded-md object-cover transition-all lg:w-3/4"
-              alt="history"
-              width={360}
-              height={267}
-              loading="eager"
-              src={
-                prevHistory?.mainImage?.node?.sourceUrl || "/images/photo-1.png"
-              }
-            />
-          </div>
-          <div className="col-span-9 max-w-[903px]">
-            <Image
-              className="aspect-video w-full rounded-md object-cover"
-              alt="history"
-              width={920}
-              height={540}
-              loading="eager"
-              src={
-                activeHistory?.mainImage?.node.sourceUrl ||
-                "/images/hero-banner.png"
-              }
-            />
-            <div className="mt-5 font-light xl:text-xl">
-              {activeHistory?.description}
-            </div>
-          </div>
+        <div
+          className="relative col-span-full gap-4 transition-all duration-300 md:col-span-8 lg:col-start-5 xl:mt-16"
+          style={{minHeight: `${imgSlideHeight}px`}}>
+          {imagePairs?.map((pair, index) => {
+            const isActive = currentSlide === index;
+            return (
+              <ImageSlide
+                key={index}
+                setImgSlideHeight={setImgSlideHeight}
+                isActive={isActive}
+                preUrl={pair?.pre?.mainImage?.node?.sourceUrl}
+                currUrl={pair?.current?.mainImage?.node.sourceUrl}
+                description={pair?.current?.description}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
   );
 };
 
+const ImageSlide = (props: {
+  isActive: boolean;
+  preUrl?: Maybe<string>;
+  currUrl?: Maybe<string>;
+  description?: Maybe<string>;
+  setImgSlideHeight: Dispatch<number>;
+}) => {
+  const {isActive, currUrl, description, preUrl, setImgSlideHeight} =
+    props || {};
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isActive) {
+      ref.current?.clientHeight && setImgSlideHeight(ref.current?.clientHeight);
+    }
+
+    window.addEventListener("resize", () => {
+      if (isActive) {
+        ref.current?.clientHeight &&
+          setImgSlideHeight(ref.current?.clientHeight);
+      }
+    });
+
+    return () =>
+      window.removeEventListener("resize", () => {
+        if (isActive) {
+          ref.current?.clientHeight &&
+            setImgSlideHeight(ref.current?.clientHeight);
+        }
+      });
+  }, [isActive]);
+
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        "absolute top-0 flex w-full flex-col gap-5 transition-opacity duration-500 xl:flex-row xl:items-start",
+        isActive ? "z-10" : "opacity-0"
+      )}>
+      <div className="relative ml-auto aspect-[316/267] max-h-52 w-1/2 overflow-hidden rounded-md xl:max-h-none xl:w-full xl:flex-1">
+        <Image
+          className="object-cover"
+          alt="history"
+          fill
+          src={preUrl || "/images/photo-1.png"}
+        />
+      </div>
+      <div className="xl:flex-[2.92]">
+        <div className="relative aspect-video  w-full overflow-hidden rounded-md ">
+          <Image
+            className="object-cover"
+            alt="history"
+            fill
+            src={currUrl || "/images/hero-banner.png"}
+          />
+        </div>
+
+        <div className="mt-5 font-light xl:text-xl">{description}</div>
+      </div>
+    </div>
+  );
+};
+
 const SlideButton = (
-  props: PropsWithChildren & {id: number; isMobile: boolean}
+  props: PropsWithChildren & {
+    id: number;
+    isActive: boolean;
+    distanceWithActive: number;
+  }
 ) => {
+  const {id, isActive, distanceWithActive} = props || {};
   const swiper = useSwiper();
 
-  const onClick = (e: any) => {
-    // if (props.isMobile) return;
-    console.log("click", props.id, swiper.activeIndex);
+  const distance = Math.abs(distanceWithActive);
 
-    swiper.slideTo(props.id);
+  const onClick = (e: any) => {
+    swiper.slideTo(id);
   };
+
   return (
-    <button className="z-10 h-full w-full" onClick={onClick}>
-      {props.children}
-    </button>
+    <div
+      className={clsx(
+        `grid h-full place-items-center transition-all duration-300`,
+        distance && "scale-75",
+        distance === 1 && "opacity-50",
+        distance === 2 && "opacity-30",
+        distance === 3 && "opacity-10",
+        distance > 3 && "opacity-0"
+      )}>
+      <button
+        className={clsx(
+          "block rounded-[5px] px-4 text-2xl text-[32px] leading-10 -tracking-wide transition-all duration-300 md:px-8 xl:text-3xl",
+          isActive ? "bg-primary-blue-main py-2 font-bold" : "font-light"
+        )}
+        onClick={onClick}>
+        <span className="inline-block translate-y-1">{props.children}</span>
+      </button>
+    </div>
   );
 };
 
