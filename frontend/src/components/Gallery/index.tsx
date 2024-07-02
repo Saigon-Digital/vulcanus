bf0fbfd6bec4dee403fb1d4c9de591555ac3db6b
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-07-02 10:52:24 +0700

    fix shape gallery

--- frontend/src/components/Gallery/index.tsx
+++ frontend/src/components/Gallery/index.tsx
@@ -10,72 +10,80 @@ const Image = dynamic(() => import("next/image"));
 const Gallery = ({title, gallery, reverseLayout}: GalleryBlock) => {
   return (
     <div className="relative py-20 lg:pb-28">
-      <div className="container-fluid grid grid-cols-12 gap-5 lg:gap-6">
-        <div className="relative col-span-full flex justify-start text-left md:col-span-3 md:justify-start 2xl:col-span-3">
-          <Image
-            src={"/shapes/left-shape.svg"}
-            width={47}
-            height={155}
-            alt="shape"
-            className="absolute -left-[25px] -top-5 hidden  md:block"
-          />
+      <div className="relative">
+        <Image
+          src={"/shapes/left-shape.svg"}
+          width={47}
+          height={155}
+          alt="shape"
+          className="absolute -left-[3px] -top-5 hidden  md:block"
+        />
 
-          <h2 className="ml-[50px] max-w-[579px] whitespace-pre-wrap break-words text-center text-2xl font-bold tracking-tight xl:text-left xl:text-3xl 2xl:w-[4/5]  3xl:text-4xl 3xl:leading-[67px]">
-            {title}
-          </h2>
-        </div>
-        <div
-          className={`col-span-full grid grid-cols-5 gap-5 md:col-span-9 2xl:col-span-9 2xl:col-start-4  `}>
-          {gallery?.map((ele, id) => {
-            return (
-              <div
-                key={id}
-                className={twMerge(
-                  "col-span-full flex flex-col gap-3 rounded-[5px] border border-primary-blue-main p-3 sm:col-span-2 lg:col-span-2 xl:p-5 ",
-                  !reverseLayout
-                    ? id === 1 || id === 2
-                      ? "lg:col-span-3"
-                      : "lg:col-span-2"
-                    : id === 0 || id === 3
-                      ? "lg:col-span-3"
-                      : "lg:col-span-2",
-                  id === 2 && gallery.length < 4 ? "lg:col-span-full" : "",
-                  ele?.textOrImge === "text" &&
-                    "border-none bg-primary-blue-100 text-primary-midBlue-main"
-                )}>
-                {ele?.textOrImge === "image" ? (
-                  <>
-                    <div className="3xl:-[500px] relative h-[200px] w-full overflow-hidden  lg:h-[300px] 2xl:h-[450px]">
-                      <motion.div
-                        whileInView={{scale: 1.05}}
-                        transition={{type: "spring", duration: 1.5, delay: 0.3}}
-                        className="relative h-full w-full">
-                        <Image
-                          fill
-                          src={ele?.galleryImage?.node?.sourceUrl || ""}
-                          className="object-cover object-center"
-                          alt="gallery image"
-                        />
-                      </motion.div>
+        <div className="container-fluid grid grid-cols-12 gap-5 lg:gap-6">
+          <div className="relative col-span-full flex justify-start text-left md:col-span-3 md:justify-start 2xl:col-span-3">
+            <h2 className="ml-[50px] max-w-[579px] whitespace-pre-wrap break-words text-center text-2xl font-bold tracking-tight xl:text-left xl:text-3xl 2xl:w-[4/5]  3xl:text-4xl 3xl:leading-[67px]">
+              {title}
+            </h2>
+          </div>
+          <div
+            className={`col-span-full grid grid-cols-5 gap-5 md:col-span-9 2xl:col-span-9 2xl:col-start-4  `}>
+            {gallery?.map((ele, id) => {
+              return (
+                <div
+                  key={id}
+                  className={twMerge(
+                    "col-span-full flex flex-col gap-3 rounded-[5px] border border-primary-blue-main p-3 sm:col-span-2 lg:col-span-2 xl:p-5 ",
+                    !reverseLayout
+                      ? id === 1 || id === 2
+                        ? "lg:col-span-3"
+                        : "lg:col-span-2"
+                      : id === 0 || id === 3
+                        ? "lg:col-span-3"
+                        : "lg:col-span-2",
+                    id === 2 && gallery.length < 4 ? "lg:col-span-full" : "",
+                    ele?.textOrImge === "text" &&
+                      "border-none bg-primary-blue-100 text-primary-midBlue-main"
+                  )}>
+                  {ele?.textOrImge === "image" ? (
+                    <>
+                      <div className="3xl:-[500px] relative h-[200px] w-full overflow-hidden  lg:h-[300px] 2xl:h-[450px]">
+                        <motion.div
+                          whileInView={{scale: 1.05}}
+                          transition={{
+                            type: "spring",
+                            duration: 1.5,
+                            delay: 0.3,
+                          }}
+                          className="relative h-full w-full">
+                          <Image
+                            fill
+                            src={ele?.galleryImage?.node?.sourceUrl || ""}
+                            className="object-cover object-center"
+                            alt="gallery image"
+                          />
+                        </motion.div>
+                      </div>
+                      <span className="text-2xl font-semibold  text-primary-blue-main">
+                        0{id + 1}
+                      </span>
+                      <p className="lg:text-lg xl:text-xl xl:leading-[28px]">
+                        {ele?.imageTitle}
+                      </p>
+                    </>
+                  ) : (
+                    <div className="[&>p]:text-lg [&>p]:leading-[24px]">
+                      <h3 className="mb-3 text-xl font-bold text-primary-midBlue-main 2xl:text-2xl">
+                        {ele?.text?.title}
+                      </h3>
+                      <p className="text-base xl:text-lg">
+                        {ele?.text?.content}
+                      </p>
                     </div>
-                    <span className="text-2xl font-semibold  text-primary-blue-main">
-                      0{id + 1}
-                    </span>
-                    <p className="lg:text-lg xl:text-xl xl:leading-[28px]">
-                      {ele?.imageTitle}
-                    </p>
-                  </>
-                ) : (
-                  <div className="[&>p]:text-lg [&>p]:leading-[24px]">
-                    <h3 className="mb-3 text-xl font-bold text-primary-midBlue-main 2xl:text-2xl">
-                      {ele?.text?.title}
-                    </h3>
-                    <p className="text-base xl:text-lg">{ele?.text?.content}</p>
-                  </div>
-                )}
-              </div>
-            );
-          })}
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
