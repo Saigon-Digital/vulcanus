8442b4dccf7e05cfaab3d338ecd6bad5f5b0d87f
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-07-02 10:26:00 +0700

    upgrade framer

--- frontend/src/components/TitleBlock/index.tsx
+++ frontend/src/components/TitleBlock/index.tsx
@@ -18,16 +18,12 @@ const TitleBlock: React.FC<TitleBlockFragment> = ({
 
   return (
     //#region
-    <section className="container-fluid relative">
+    <section className=" relative">
       <div
-        className={`title-block   grid grid-cols-12 py-16 lg:py-20 ${
+        className={`title-block container-fluid  grid grid-cols-12 py-16 lg:py-20 ${
           haveBorderBottom && " border-b border-[#E6ECF3]"
         }`}>
-        <motion.div
-          style={{y: 50, opacity: 0}}
-          whileInView={{y: 0, opacity: 1}}
-          viewport={{margin: "50%"}}
-          transition={{duration: 0.8, delay: 0.4}}
+        <div
           ref={titleRef}
           className={clsx(
             "title col-span-full max-w-[1565px]  font-bold md:col-span-10 xl:text-6xl 3xl:col-span-9 [&>*]:text-4xl [&>*]:tracking-tight ",
@@ -35,12 +31,12 @@ const TitleBlock: React.FC<TitleBlockFragment> = ({
               ? "2xl:leading-[76px] 3xl:leading-[89px] 3xl:[&>*]:text-[64px] 3xl:[&>*]:leading-[89px]"
               : "2xl:leading-[67px] xl:[&>*]:text-5xl 2xl:[&>*]:leading-[67px]"
           )}
-          dangerouslySetInnerHTML={{__html: title || ""}}></motion.div>
+          dangerouslySetInnerHTML={{__html: title || ""}}></div>
         {haveShape && (
           <div>
             <TitleShape
-              h={200}
-              w={200}
+              h={150}
+              w={150}
               className="absolute -right-[10px] top-1/2 hidden -translate-y-1/2 lg:block"
             />
           </div>
--- frontend/src/components/TitleText/index.tsx
+++ frontend/src/components/TitleText/index.tsx
@@ -10,24 +10,23 @@ const index = ({
   title = "We are committed to innovation, quality, and meeting the evolving needs of our clients.",
 }: TText) => {
   return (
-    <section className="container-fluid relative  gap-4 py-14">
-      <div className="relative grid grid-cols-12">
-        <motion.h2
-          style={{y: "50px", opacity: 0}}
-          whileInView={{y: 0, opacity: 1}}
-          viewport={{margin: "40%"}}
-          transition={{duration: 0.8, delay: 0.4}}
-          className="title-xl col-span-10 md:col-span-8 lg:col-span-7">
-          {title}
-        </motion.h2>
-        <Image
-          height={233}
-          width={210}
-          alt="shape"
-          src="/images/shape.svg"
-          className="absolute right-0 hidden aspect-square h-1/3 object-contain sm:top-1/2 sm:h-2/3 sm:-translate-y-1/2 md:block lg:h-full"
-        />
+    <section className=" relative  gap-4 py-14">
+      <div className="container-fluid">
+        <div className="relative grid grid-cols-12">
+          {title && (
+            <h2 className="title-xl col-span-10 md:col-span-8 lg:col-span-7">
+              {title}
+            </h2>
+          )}
+        </div>
       </div>
+      <Image
+        height={233}
+        width={210}
+        alt="shape"
+        src="/images/shape.svg"
+        className="absolute right-0 hidden aspect-square h-1/3 object-contain sm:top-1/2 sm:h-2/3 sm:-translate-y-1/2 md:block lg:h-full"
+      />
     </section>
   );
 };
