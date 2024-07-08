ea79992fda8f3213f89eda2937832ef2581595f1
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-07-08 08:52:57 +0700

    image slide loadding priority

--- frontend/src/components/ImageWithRatio/index.tsx
+++ frontend/src/components/ImageWithRatio/index.tsx
@@ -1,21 +1,22 @@
-import {useRatio} from "@/hooks/useRatio";
-import Image from "next/image";
-import {twMerge} from "tailwind-merge";
+import {useRatio} from "@/hooks/useRatio"
+import Image from "next/image"
+import {twMerge} from "tailwind-merge"
 
 type Props = {
-  imageSrc?: string | null | undefined;
-  className?: string;
-  height?: number;
-  width?: number;
-};
+  imageSrc?: string | null | undefined
+  className?: string
+  height?: number
+  width?: number
+}
 const ImageWithRatio = ({imageSrc, height = 400, className, width}: Props) => {
-  const ratio = useRatio(imageSrc || "");
-  const w = ratio && height ? height * ratio : height;
+  const ratio = useRatio(imageSrc || "")
+  const w = ratio && height ? height * ratio : height
 
   return (
     <Image
       width={w}
       height={height}
+      priority
       style={{
         height: height || "auto",
         width: width || "auto",
@@ -25,7 +26,7 @@ const ImageWithRatio = ({imageSrc, height = 400, className, width}: Props) => {
       alt="image auto ratio"
       src={imageSrc || ""}
     />
-  );
-};
+  )
+}
 
-export default ImageWithRatio;
+export default ImageWithRatio
