1e3b60b703bffe833331446120d00d6b89053017
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-07-12 17:21:39 +0700

    update lang toggle

--- frontend/src/components/Header/LanguageToggle.tsx
+++ frontend/src/components/Header/LanguageToggle.tsx
@@ -23,6 +23,7 @@ const LanguageButton = ({
     <Link
       href={href}
       aria-disabled={disabled}
+      locale={label === "ENG" ? "en" : "de"}
       // locale={locale}
       className={twMerge(
         "inline-block rounded-full px-2 pb-[7.65px] pt-[9px] text-center text-base font-semibold uppercase leading-[140%] xl:px-[14.7px] xl:text-[18px]",
@@ -36,9 +37,12 @@ const LanguageButton = ({
 }
 type Props = {}
 
+const commingSoonLink = "/coming-soon"
+
 const LanguageToggle = (props: Props) => {
-  const {localeData} = useLocaleContext()
+  const {localeData, asPath} = useLocaleContext()
   const router = useRouter()
+  // console.log(asPath)
 
   const handleLanguageChange = useCallback(
     (locale: string) => {
@@ -59,14 +63,14 @@ const LanguageToggle = (props: Props) => {
     <div className="rounded-full border border-primary-blue-main bg-primary-blue-100 px-[3.68px] py-[3px]">
       <LanguageButton
         label="ENG"
-        href={"/en/coming-soon"}
+        href={`/${commingSoonLink}`}
         // disabled={localeData ? !localeData["EN"] !== null : true}
         isActive={router.locale === "en"}
         // onClick={() => handleLanguageChange("EN")}
       />
       <LanguageButton
         label="GER"
-        href={localeData?.DE || ""}
+        href={asPath === commingSoonLink ? "/" : localeData?.DE || ""}
         // disabled={localeData ? !localeData["DE"] !== null : true}
         isActive={router.locale === "de"}
         // onClick={() => handleLanguageChange("DE")}
