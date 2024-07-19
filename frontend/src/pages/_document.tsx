cfff3ae1d588cd7c117b9b486e679eb8ed274b7e
phuocMinhnguyen123 <126453986+phuocMinhnguyen123@users.noreply.github.com>
2024-07-19 15:14:00 +0700

    add noscript

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import {Html, Head, Main, NextScript} from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,15 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJT5DR2G" height={0} width={0}
+            style={{ display: 'none', visibility: 'hidden' }} />
+
+        </noscript>
+
+
       </body>
     </Html>
   );
