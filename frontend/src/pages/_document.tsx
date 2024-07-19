38405ed8b22d051cfe078780952353ea75fb7659
duynd243 <duyndse@gmail.com>
2024-07-19 15:18:42 +0700

    chore: Update _document.tsx to fix Google Tag Manager integration

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import {Html, Head, Main, NextScript} from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
@@ -9,15 +9,16 @@ export default function Document() {
         <Main />
         <NextScript />
 
-
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJT5DR2G" height={0} width={0}
-            style={{ display: 'none', visibility: 'hidden' }} />
-
+          <iframe
+            title="Google Tag Manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJT5DR2G"
+            height={0}
+            width={0}
+            style={{display: "none", visibility: "hidden"}}
+          />
         </noscript>
-
-
       </body>
     </Html>
-  );
+  )
 }
