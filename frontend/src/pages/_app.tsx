0946180a6a3c2a2f88ba8b6e8be3a67fa9348a2e
duynd243 <duyndse@gmail.com>
2024-07-19 15:06:05 +0700

    chore: gtag and ga4

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,25 +1,25 @@
 import dynamic from "next/dynamic"
-import { FaustProvider } from "@faustwp/core"
-import type { AppProps } from "next/app"
-import { useRouter } from "next/router"
+import {FaustProvider} from "@faustwp/core"
+import type {AppProps} from "next/app"
+import {useRouter} from "next/router"
 import "../../faust.config"
 import "swiper/css"
 import "swiper/css"
 import "swiper/css/pagination"
 import "@/styles/globals.scss"
 
-import { LocaleContextProvider } from "@/context/LocaleContext"
-import { AnimatePresence, LazyMotion } from "framer-motion"
-import { Overpass } from "next/font/google"
-import { ApolloProvider } from "@apollo/client"
-import { client } from "@/libs/graphql/utils"
-import { ModalContext, ModalContextProvider } from "@/context/modalContext"
-import { TextContent, TextContentProvider } from "@/context/textContent"
+import {LocaleContextProvider} from "@/context/LocaleContext"
+import {AnimatePresence, LazyMotion} from "framer-motion"
+import {Overpass} from "next/font/google"
+import {ApolloProvider} from "@apollo/client"
+import {client} from "@/libs/graphql/utils"
+import {ModalContext, ModalContextProvider} from "@/context/modalContext"
+import {TextContent, TextContentProvider} from "@/context/textContent"
 import Head from "next/head"
 import Script from "next/script"
 const Layout = dynamic(() => import("@/components/Layout"))
-import CookieConsent, { Cookies } from "react-cookie-consent"
-import { languages } from "@/utils/language"
+import CookieConsent, {Cookies} from "react-cookie-consent"
+import {languages} from "@/utils/language"
 //font
 const overpass = Overpass({
   subsets: ["latin"],
@@ -28,7 +28,7 @@ const overpass = Overpass({
   preload: true,
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
   const router = useRouter()
 
   // console.log("page data ", page);
@@ -48,17 +48,20 @@ export default function App({ Component, pageProps }: AppProps) {
           content="initial-scale = 1.0,maximum-scale = 1.0"
         />
       </Head>
-      <Script>{`
-        <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-6BCK1XNJXY"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-6BCK1XNJXY"
+      />
+      <Script
+        id="google-tag"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', 'G-6BCK1XNJXY');
-            </script>
-        `}</Script>
+              gtag('config', 'G-6BCK1XNJXY');`,
+        }}
+      />
       <FaustProvider pageProps={pageProps}>
         <LocaleContextProvider
           localeData={{
@@ -79,8 +82,6 @@ export default function App({ Component, pageProps }: AppProps) {
                   }
                   key={`${router.asPath}-${router.locale}`}>
                   {/* > */}
-
-
 
                   <Component
                     {...pageProps}
