6149ddf1df1f2887260fc21228c25ebeeccc713c
Minh Tri <minhtri0795@gmail.com>
2025-02-19 17:21:59 +0700

    feat: ui rework Gallery block

--- frontend/src/components/Gallery/index.tsx
+++ frontend/src/components/Gallery/index.tsx
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic"
 import {twMerge} from "tailwind-merge"
 import {useRouter} from "next/router"
 import {allLowercase} from "@/utils"
+import Link from "next/link"
 
 const Image = dynamic(() => import("next/image"))
 const zoomOut = []
@@ -54,7 +55,7 @@ const Gallery = ({title, gallery, reverseLayout}: GalleryBlock) => {
   return (
     <div ref={ref} className="relative py-14 md:py-20 lg:pb-28">
       <div className="relative">
-        <img
+        <Image
           src={"/shapes/left-shape.svg"}
           width={47}
           height={155}
@@ -63,33 +64,47 @@ const Gallery = ({title, gallery, reverseLayout}: GalleryBlock) => {
         />
 
         <div className="container-fluid grid grid-cols-12 gap-5 gap-y-6 lg:gap-6">
-          <div className="relative col-span-full flex justify-start text-left md:justify-start xl:col-span-3 2xl:col-span-3">
-            <h2 className="max-w-[579px] whitespace-pre-wrap break-words text-left  text-2xl font-bold  tracking-tight md:ml-[50px] xl:ml-10 xl:text-left xl:text-2xl 2xl:w-[4/5] 2xl:text-3xl  3xl:text-4xl 3xl:leading-[67px]">
+          <div className="relative col-span-full flex flex-col justify-start text-left md:justify-start xl:col-span-3 2xl:col-span-3">
+            <h2 className="max-w-[579px] text-left text-3xl font-bold md:ml-[50px] xl:ml-10 xl:text-left xl:text-5xl xl:leading-[67px] 2xl:w-[4/5]">
               {title}
             </h2>
+            <div className="md:ml-[50px] xl:ml-10 mb-10 xl:mb-0">
+              {gallery?.map((ele, id) => {
+                if (ele?.textOrImge === "text")
+                return (
+                  <div className="" key={id}>
+                    <h2 className="mb-8 text-xl font-bold text-primary-blue-200">
+                      {ele?.text?.title}
+                    </h2>
+                    <p className="text-primary-blue-200">
+                      {ele?.text?.content}
+                    </p>
+                  </div>
+                )
+              })}
+              <Link
+                href="#"
+                className="mt-8 xl:mt-16 fit-content inline-flex w-[fit-content] items-center justify-center whitespace-nowrap bg-primary-blue-main px-10 py-[17px] text-center font-bold uppercase leading-[125%] text-secondary-offWhite-white transition-all duration-300 hover:bg-primary-blue-400 lg:px-[60px]">
+                Mehr Über Uns
+              </Link>
+            </div>
           </div>
           <div
             className={`col-span-full grid grid-cols-6 gap-5 md:ml-[50px] lg:col-span-11  lg:grid-cols-8  xl:col-span-8 xl:col-start-5 xl:ml-0    `}>
             {gallery?.map((ele, id) => {
-              return (
-                <div
-                  key={id}
-                  className={twMerge(
-                    "col-span-full flex flex-col justify-start gap-3 rounded-[5px] border  border-primary-blue-main p-3 sm:col-span-3 sm:p-3 lg:col-span-2 xl:p-5 3xl:p-6 ",
-                    !reverseLayout
-                      ? id === 1 || id === 2
-                        ? "lg:col-span-5"
-                        : "lg:col-span-3 "
-                      : id === 0 || id === 3
-                        ? "lg:col-span-5"
-                        : "lg:col-span-3",
-                    id === 2 && gallery.length < 4 ? "lg:col-span-full" : "",
-                    ele?.textOrImge === "text" &&
-                      "border-none bg-primary-blue-100 text-primary-midBlue-main"
-                  )}>
-                  {ele?.textOrImge === "image" ? (
+              if (ele?.textOrImge === "image")
+                return (
+                  <div
+                    key={id}
+                    className={twMerge(
+                      "col-span-full flex flex-col justify-start gap-3 rounded-[5px] border  border-primary-blue-main p-3 sm:col-span-3 sm:p-3 lg:col-span-2 xl:p-5 3xl:p-6 ",
+                      id === 0 && "lg:col-span-3",
+                      id === 1 && "lg:col-span-5",
+
+                      id === 2 && "lg:col-span-8"
+                    )}>
                     <>
-                      <div className="relative h-[390px]  w-full overflow-hidden  lg:h-[420px] 2xl:h-[500px] 3xl:h-[550px] ">
+                      <div className="relative h-[352px]  w-full overflow-hidden">
                         <div className="relative h-full w-full">
                           <Image
                             fill
@@ -106,18 +121,8 @@ const Gallery = ({title, gallery, reverseLayout}: GalleryBlock) => {
                         {ele?.imageTitle}
                       </p>
                     </>
-                  ) : (
-                    <div className="[&>p]:text-lg [&>p]:leading-[24px]">
-                      <h3 className="mb-3 text-xl font-bold text-primary-midBlue-main 2xl:text-2xl">
-                        {ele?.text?.title}
-                      </h3>
-                      <p className="text-base xl:text-lg">
-                        {ele?.text?.content}
-                      </p>
-                    </div>
-                  )}
-                </div>
-              )
+                  </div>
+                )
             })}
           </div>
         </div>
